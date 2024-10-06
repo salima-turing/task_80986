8e0708e2a4089a02d1f1ed4d1dc226cf8edcdfdd
salima-turing <salima.o@turing.com>
2024-10-06 13:43:38 +0100

    Create after_linting.py

---
+++ Turn 2/Model A/after_linting.py
@@ -0,0 +1,13 @@
+# Example Python Code after Linting
+
+import math
+
+def calculate_area(radius):
+   pi = math.pi
+   area = pi * radius * radius
+   return area
+
+print(calculate_area(5))
+
+if __name__ == "__main__":
+   print("Hello, World!")
