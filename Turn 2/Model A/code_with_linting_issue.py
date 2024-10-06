fca33a5515c70e740adf6e0cd07f1fcc88dc3795
salima-turing <salima.o@turing.com>
2024-10-06 13:31:23 +0100

    Create code_with_linting_issue.py

---
+++ Turn 2/Model A/code_with_linting_issue.py
@@ -0,0 +1,11 @@
+# Example Python Code with Linting Issues
+
+def calculate_area(radius):
+	pi = 3.14
+	area = pi * raius * raius  # Indentation error and 'raius' should be 'radius'
+	return area
+
+print(calculate_area(5))  # Missing semicolon
+
+if __name__ == "__main__":
+	print("Hello World")
